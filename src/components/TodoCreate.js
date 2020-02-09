2f098147c9e018d820151d28f61db360494ca453
goforitrj <eleveniasjr@gmail.com>
2020-02-09 15:33:55 +1100

    Add input area to TodoCreate component

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -40,13 +40,42 @@ const CircleButton = styled.button`
             transform: translate(-50%, 50%) rotate(45deg);
         `}
 `;
+
+const InsertForm = styled.div`
+    background: #f8f9fa;
+    padding: 32px;
+    padding-bottom: 72px;
+
+    border-bottom-left-radius: 16px;
+    border-bottom-right-radius: 16px;
+    border-top: 1px solid #e9ecef;
+`;
+const Input = styled.input`
+    padding: 12px;
+    border-radius: 4px;
+    border: 1px solid #dee2e6;
+    width: 100%;
+    outline: none;
+    font-size: 18px;
+    box-sizing: border-box;
+`;
 function TodoCreate() {
     const [open, setOpen] = useState(false);
     const onToggle = () => setOpen(!open);
     return (
-        <CircleButton onClick={onToggle} open={open}>
-            <MdAdd />
-        </CircleButton>
+        <>
+            {open && (
+                <InsertForm>
+                    <Input
+                        autoFocus
+                        placeholder="Press Enter, after creating new todo item"
+                    />
+                </InsertForm>
+            )}
+            <CircleButton onClick={onToggle} open={open}>
+                <MdAdd />
+            </CircleButton>
+        </>
     );
 }
 
