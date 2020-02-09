bb26b707d1de085c90efd862d2c5bd5e140967db
goforitrj <eleveniasjr@gmail.com>
2020-02-09 15:18:29 +1100

    Style for Creat Button using position(relative, absolute), display(flex), transform, translate

---
+++ src/components/TodoCreate.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { MdAdd } from 'react-icons/md';
+
+const CircleButton = styled.button`
+    background: #38d9a9;
+    &:hover {
+        background: #63e6be;
+    }
+    &:active {
+        background: #20c997;
+    }
+    cursor: pointer;
+    width: 80px;
+    height: 80px;
+    font-size: 60px;
+    position: absolute;
+    left: 50%;
+    bottom: 0px;
+    transform: translate(-50%, 50%);
+    color: white;
+    border-radius: 50%;
+    border: none;
+    outline: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: 0.125s all ease-in;
+    ${props =>
+        props.open &&
+        css`
+            background: #ff6b6b;
+            &:hover {
+                background: #ff8787;
+            }
+            &:active {
+                background: #fa5252;
+            }
+            transform: translate(-50%, 50%) rotate(45deg);
+        `}
+`;
+function TodoCreate() {
+    const [open, setOpen] = useState(false);
+    const onToggle = () => setOpen(!open);
+    return (
+        <CircleButton onClick={onToggle} open={open}>
+            <MdAdd />
+        </CircleButton>
+    );
+}
+
+export default TodoCreate;
